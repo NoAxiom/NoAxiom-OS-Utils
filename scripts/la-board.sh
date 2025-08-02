622a65879a7013d09d15afba4048a48556fc1fd4
crpboy <crpboy2004@gmail.com>
2025-08-02 20:04:45 +0800

    feat(board): add shell scripts for la phys board test

---
+++ scripts/la-board.sh
@@ -0,0 +1,14 @@
+kernel_bin=../../kernel-la-uimage
+sim_path=../la-2k1000-sim
+tftp_path=/mnt/d/tftp
+boot_command_path=scripts/uboot-commands.sh
+
+echo "Building uImage for LA architecture..."
+cd ${sim_path} && make uimage
+mkdir -p ${tftp_path}
+
+echo "Copying uImage to TFTP path to to ${tftp_path}..."
+cp ${kernel_bin} ${tftp_path}/uImage
+
+echo "UImage for LA architecture is ready at ${tftp_path}/uImage"
+echo "See boot commands in ${boot_command_path}"
